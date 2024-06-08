c2898efaeaadaa25774c25011327d56c54b2f552
Marc (XPS) <marc@webmarcit.com>
2024-06-08 21:30:31 +0100

    Flake8 updates

--- python/scorch.py
+++ python/scorch.py
@@ -20,49 +20,56 @@ import logging
 import time
 
 LOG_FORMAT = "%(asctime)s %(levelname)-8s - %(message)s"
-logging.basicConfig(filename = "/tmp/lumberjack.log",
-                     format = LOG_FORMAT,
-                     level = logging.DEBUG )
+logging.basicConfig(filename="/tmp/lumberjack.log",
+                    format=LOG_FORMAT,
+                    level=logging.DEBUG)
 
-                     #format = LOG_FORMAT,
-                     #filemode = "w")
+# format = LOG_FORMAT,
+# filemode = "w")
 
-#logger = logging.getlogger()
+# logger = logging.getlogger()
 
 # Test messages
-#logging.debug('This is a DEBUG message')
-#logging.info('Well thats just INFO')
-#logging.warning('Careful now. Thats a WARNING')
-#logging.error('I told you to be careful - Youve ERRORed')
-#logging.critical('XXX XXX XXX CRITICAL XXX XXX XXX')
+# logging.debug('This is a DEBUG message')
+# logging.info('Well thats just INFO')
+# logging.warning('Careful now. Thats a WARNING')
+# logging.error('I told you to be careful - Youve ERRORed')
+# logging.critical('XXX XXX XXX CRITICAL XXX XXX XXX')
 
 global warncount
 warncount = 0
+
+
 def warn(msg):
-  logging.warning(msg)
-  #warncount += 1
+    logging.warning(msg)
+    # warncount += 1
+
 
 def yesno(prompt):
-  '''returns True if answer is y or Y'''
-  answer = input(prompt + " (Y/N): ")
-  return answer.strip().lower() == "y"
+    '''returns True if answer is y or Y'''
+    answer = input(prompt + " (Y/N): ")
+    return answer.strip().lower() == "y"
+
 
 Debug = logging.debug
 Message = logging.info
-Warning  = logging.warning
+Warning = logging.warning
 Error = logging.error
 Critical = logging.critical
 Debug('Hello')
 Warning('This is a warning')
 warn('This is also a warn warning')
+
 if yesno('Did this work?'):
-  print("It's a yes from me")
+    print("It's a yes from me")
 else:
-  print("That's a shame")
+    print("That's a shame")
 
 job_status = 'unknown'
 wait_time = 20
+
 while True:
-  logging.info('Job status: {}'.format(job_status))
-  if job_status == 'complete' : break
-  time.sleep(wait_time)
+    logging.info('Job status: {}'.format(job_status))
+    if job_status == 'complete':
+        break
+    time.sleep(wait_time)
